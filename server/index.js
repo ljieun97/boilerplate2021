8bd229361f1833a3ff8c76420d9b02755c7d96bc
이지은 <wldms8196@gmail.com>
2021-03-19 18:02:11 +0900

    create-react-app

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const port = 5000
+
+const config = require('./config/dev')
+
+const mongoose = require("mongoose")
+mongoose
+.connect(config.mongoURI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true, 
+    useFindAndModify: false
+    })
+.then(()=>console.log('MongoDB Connected...'))
+.catch(err=>console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ server/models/User.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 100
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 10
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+//회원가입 시 디비에 저장될 비밀번호 암호화
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+    
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
+//로그인 시 비밀번호를 암호화해서 디비에 저장된 비밀번호와 비교
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+//로그인 시 토큰 생성
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+
+//인증 시 토큰과 디비의 토큰을 복호화하여 비교
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        user.findOne({"_id": decoded, "token": token}, function(err, user) {
+            if(err) return cb(err)
+            cb(null, user)
+        })
+    })
+
+}
+
+const User = mongoose.model('User', userSchema)
+module.exports = {User}
