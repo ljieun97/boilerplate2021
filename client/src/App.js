290c170243590699f74c1f609292f0198db3ce59
이지은 <wldms8196@gmail.com>
2021-03-22 18:38:04 +0900

    login

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
+  Route
 } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './routes/Home';
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,22 +2,23 @@ import React from "react";
 import {
     Link
   } from "react-router-dom";
+import KakaoLoginButton from "./KakaoLoginButton";
 
 import "./Navbar.css"
 
 function Navbar() {
     return (
         <div>
-            <ul class="navbar">
+            <ul className="navbar">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/menu1">menu1</Link></li>
                 <li><Link to="/menu2">menu2</Link></li>
-                <li><button>more</button>
-                    <ul>
-                        <li><Link to="/profile">Profile</Link></li>
-                        <li><button>logout</button></li>
-                    </ul>
-                </li>
+                <li><Link to="/profile">Profile</Link></li>
+                <li><button>sign in/sign up</button>
+                <ul>
+                        <li><KakaoLoginButton /></li>
+                        <li><button>Kakao</button></li>
+                    </ul></li>
             </ul>
         </div>
     );
